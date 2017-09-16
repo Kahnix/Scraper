4cd5414c8a47a0bf84caccc7e16dac907eed02b4
Kacper Daniel <kacperdev@gmail.com>
2017-09-16 23:18:25 +0100

    Updated Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -1,4 +1,4 @@
 def scrape(link,format,pagestart,pagefin,):
 	for i in range(pagestart, pagefin):	
 		urllib.request.urlretrieve("{}{0:03d}{}".format(link,i,format))
-		print("Downloading " + "{0:03d}".format(i))
+		print("Downloading {0:03d}".format(i))
