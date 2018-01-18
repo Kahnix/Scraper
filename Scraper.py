85f7c4d9bc980f75c3f95e81b0b4c4e47485cc43
brigdan <kacperdev@gmail.com>
2018-01-18 15:02:00 +0000

    Fixed code, reformatted pagenumber

--- Scraper.py
+++ Scraper.py
@@ -1,4 +1,7 @@
+import urllib.request
 def scrape(link,format,pagestart,pagefin):
-	for i in range(pagestart, pagefin):	
-		urllib.request.urlretrieve("{}{0:03d}{}".format(link,i,format))
-		print("Downloading {0:03d}".format(i))
+	for i in range(pagestart, pagefin):
+		pagenumber = '{0:03d}'.format(i)
+		url=("{}{}{}".format(link,pagenumber,format))
+		urllib.request.urlretrieve(url,pagenumber+format)
+		print("Downloading " + "{0:03d}".format(i) +" Out of " + str(pagefin))
